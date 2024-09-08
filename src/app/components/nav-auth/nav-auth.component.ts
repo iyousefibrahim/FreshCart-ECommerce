8401b0ce3729d92cba4dda2fff9ad1f404fe918a
Yousef <ioyx515@gmail.com>
2024-09-08 19:26:37 +0300

    "Refactored Nav Auth component HTML and TS files: reformatted code, added NgClass import, and updated CSS classes for language dropdown and navigation."

--- src/app/components/nav-auth/nav-auth.component.ts
+++ src/app/components/nav-auth/nav-auth.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MyTranslateService } from '../../core/services/my-translate.service';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-nav-auth',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, TranslateModule],
+  imports: [RouterLink, RouterLinkActive, TranslateModule,NgClass],
   templateUrl: './nav-auth.component.html',
   styleUrl: './nav-auth.component.scss'
 })
